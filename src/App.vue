6d80a0c49db4e07eb179fa2c3ff97f63db0d7bd2
parkpoomd <parkpoom.d@gmail.com>
2022-06-28 17:05:35 +0700

    version: 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-10">
+    <ul>
+      <node-item v-for="node in nodes" :key="node.id" :node="node"></node-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NodeItem from './components/NodeItem.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() {
+    return {
+      nodes: [
+        {
+          label: 'Folder 1',
+          checked: false,
+          children: [
+            {
+              label: 'Folder 1.1',
+              checked: false,
+              children: [{ label: 'Folder 1.1.1', checked: false }],
+            },
+          ],
+        },
+        {
+          label: 'Folder 2',
+          checked: false,
+          children: [
+            {
+              label: 'Folder 2.1',
+              checked: false,
+            },
+            {
+              label: 'Folder 2.2',
+              checked: false,
+            },
+            {
+              label: 'Folder 2.3',
+              checked: false,
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  created() {
+    // fetch('https://jsonplaceholder.typicode.com/photos')
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.nodes = [
+    //       {
+    //         id: 12494,
+    //         label: 'Folder 1',
+    //         checked: false,
+    //         children: json
+    //           .map((element) => ({
+    //             id: element.id,
+    //             label: element.title.substring(0, 10),
+    //             checked: false,
+    //           }))
+    //           .slice(0, 500),
+    //       },
+    //     ];
+    //   });
+  },
+
+  components: { NodeItem },
+};
+</script>
---
+++ src/components/NodeItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <li class="list-none">
+    <div class="flex items-center">
+      <button
+        type="button"
+        class="mr-2"
+        v-if="hasChildren"
+        @click="handleClickParent"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+
+      <div :class="`${!hasChildren ? 'pl-7' : ''}`">
+        <label class="inline-flex items-center">
+          <input
+            type="checkbox"
+            :checked="node.checked"
+            @input="handleClickNode(node)"
+          />
+          <span class="ml-2">
+            {{ node.label }}
+          </span>
+        </label>
+      </div>
+    </div>
+
+    <ul v-if="hasChildren && showChildren" class="pl-6">
+      <node-item
+        v-for="child in node.children"
+        :key="child.id"
+        :node="child"
+      ></node-item>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'NodeItem',
+
+  props: {
+    node: Object,
+  },
+
+  data() {
+    return {
+      showChildren: false,
+    };
+  },
+
+  computed: {
+    hasChildren() {
+      return this.node.children && this.node.children.length;
+    },
+  },
+
+  methods: {
+    handleClickParent() {
+      this.showChildren = !this.showChildren;
+    },
+    handleClickNode(node) {
+      // console.log(node.label);
+      this.recursive(node);
+    },
+    recursive(node) {
+      // console.log(node);
+      if (node.children && node.children.length) {
+        // console.log('if');
+        node.children.forEach((n) => {
+          n.checked = !n.checked;
+          this.recursive(n);
+        });
+      }
+    },
+  },
+};
+</script>
